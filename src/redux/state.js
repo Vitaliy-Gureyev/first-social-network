332be3f3f851e2333f849906f135990e695f0e34
Vitaliy Hureyev <hbeatprod@gmail.com>
2021-02-23 19:22:11 +0200

    adding state.js

---
+++ src/redux/state.js
@@ -0,0 +1,35 @@
+let state = {
+    profilePage: {
+        posts: [
+            {id: '1', message: 'Hello', count: '12'},
+            {id: '2', message: 'How are you', likesCount: '11'},
+            {id: '3', message: 'Life is great', likesCount: '0'},
+            {id: '4', message: 'Sun shine', likesCount: '5'},
+            {id: '5', message: 'Yo', likesCount: '12'},
+            {id: '6', message: 'Wats up', likesCount: '12'},
+            {id: '7', message: 'Kiks', likesCount: '12'},
+        ],
+    },
+    dialogsPage: {
+        messagesData: [
+            {id: '1', message: 'Hello'},
+            {id: '2', message: 'How are you'},
+            {id: '3', message: 'Life is great'},
+            {id: '4', message: 'Sun shine'},
+            {id: '5', message: 'Я хочу выпить'},
+            {id: '6', message: 'Куда пойдем?'},
+            {id: '7', message: 'Кикс'}
+        ],
+        dialogsData: [
+            {id: '1', name: 'Vitalik'},
+            {id: '2', name: 'Yana'},
+            {id: '3', name: 'Anya'},
+            {id: '4', name: 'Kolya'},
+            {id: '5', name: 'Nastya'},
+            {id: '6', name: 'Marina'},
+            {id: '7', name: 'VitalikR'},
+        ],
+    }
+}
+
+export default state
