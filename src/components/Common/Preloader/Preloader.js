beb149e19897656c13a8440cfa1486ca7b65bcc6
Vitaliy Hureyev <hbeatprod@gmail.com>
2021-03-13 21:42:41 +0200

    preloader

---
+++ src/components/Common/Preloader/Preloader.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+let Preloader = (props) => {
+    return <div>
+        <img src={'https://i.ibb.co/0KQ5L2d/22.gif'}/>
+    </div>
+}
+
+export default Preloader;
--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -3,11 +3,14 @@ const UNFOLLOW = 'UNFOLLOW';
 const SET_USERS = 'SET_USERS';
 const SET_CURRENT_PAGE = 'SET_CURRENT_PAGE';
 const SET_USERS_TOTAL_COUNT = 'SET_USERS_TOTAL_COUNT';
+const TOGGLE_IS_FETCHING = 'TOGGLE_IS_FETCHING';
+
 let initialState = {
     users: [],
     pageSize: 100,
     totalUsersCount: 0,
-    currentPage: 2
+    currentPage: 2,
+    isFetching: true
 };
 const usersReducer = (state = initialState, action) => {
 
@@ -32,7 +35,6 @@ const usersReducer = (state = initialState, action) => {
                     return u;
                 })
             };
-
         case SET_USERS: {
             return {...state, users: action.users}
         }
@@ -41,6 +43,9 @@ const usersReducer = (state = initialState, action) => {
         }
         case SET_USERS_TOTAL_COUNT: {
             return {...state, totalUsersCount: action.count}
+        }
+        case TOGGLE_IS_FETCHING: {
+            return {...state, isFetching: action.isFetching}
         }
         default:
             return state;
@@ -54,5 +59,6 @@ export const unfollowAC = (userId) => ({type: UNFOLLOW, userId});
 export const setUsersAC = (users) => ({type: SET_USERS, users});
 export const setCurrentPageAC = (currentPage) => ({type: SET_CURRENT_PAGE, currentPage});
 export const setUsersTotalCountAC = (totalUsersCount) => ({type: SET_USERS_TOTAL_COUNT, count: totalUsersCount});
+export const toggleIsFetchingAC = (isFetching) => ({type: TOGGLE_IS_FETCHING, isFetching});
 
 export default usersReducer;
